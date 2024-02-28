6943d56f12db64d5ce659b748c16d62d3b201851
Irin Rahman <145903406+rahmanrosne@users.noreply.github.com>
2024-02-28 16:57:54 +0600

    Add files via upload

---
+++ ariyan/assets/js/main.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+    alart("display alart")
+})
+
+// $(".show").click(function(){
+//     $(".box").css("opacity","100%")
+// })
+
+// $(".show").click(function(){
+//     $(".box").css("opacity","0")
+// })
+
+$(".box").css({"background":"red","border-radius":"5px"})
